110a7fd4c1a2426dbea0cd4d03925073c036c52d
ka1sser <serenokaisser.dev@gmail.com>
2025-08-06 01:31:21 +0800

    Added animation for hero page and dynamic handling introduction using js

--- 02-js-interactive/scripts/index.js
+++ 02-js-interactive/scripts/index.js
@@ -1,15 +1,40 @@
-const pages = document.querySelectorAll(".page");
+document.addEventListener('DOMContentLoaded', () => {
+  const hero = document.getElementById('hero');
+  const h2 = hero.querySelector('h2');
+  const h1s = hero.querySelectorAll('h1');
+  const span = hero.querySelector('span');
+  const nav = document.querySelector('nav');
 
-// IntersectionObserver to detect when pages enter viewport
-const observer = new IntersectionObserver(
-  (entries) => {
-    entries.forEach((entry) => {
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+
       if (entry.isIntersecting) {
-        entry.target.classList.add("visible");
-    }
-    });
-  },
-  { threshold: 0.35 }
-);
+        h2.classList.remove('fade-in-h2');
+        h1s.forEach(h1 => h1.classList.remove('fade-in-h1'));
+        span.classList.remove("fade-in-span");
+        nav.classList.remove('fade-in-nav');
 
-pages.forEach((page) => observer.observe(page));
+        setTimeout(() => {
+          h2.classList.add('fade-in-h2');
+        }, 0);
+
+        setTimeout(() => {
+          h1s.forEach(h1 => h1.classList.add('fade-in-h1'));
+        }, 1500);
+
+        setTimeout(() => {
+          span.classList.add("fade-in-span");
+          nav.classList.add('fade-in-nav');
+        }, 3000);
+      } else {
+
+        h2.classList.remove('fade-in-h2');
+        h1s.forEach(h1 => h1.classList.remove('fade-in-h1'));
+        span.classList.remove('fade-in-span');
+        nav.classList.remove('fade-in-nav');
+      }
+    });
+  }, { threshold: 0.5 });
+
+  observer.observe(hero);
+});
